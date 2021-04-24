d83a77e51e75914df6972fc49689709932ee68cf
ste <s.cadeck@gmail.com>
2021-04-24 12:40:47 +0200

    Update menu and add scrollText.js

---
+++ scrollText.js
@@ -0,0 +1,15 @@
+(function () {
+    var fig = document.querySelectorAll("figure")[0];
+    var posImg = fig.offsetTop;
+    function effet() {
+        var posCurseur = this.pageYOffset;
+        if (posImg - posCurseur < 800) {
+            fig.style.left = 0;
+            fig.style.opacity = 1;
+        } else {
+            fig.style.left = "160%";
+            fig.style.opacity = 0;
+        }
+    }
+    window.addEventListener("scroll", effet);
+})();
