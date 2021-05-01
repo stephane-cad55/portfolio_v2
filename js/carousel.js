2de7ee37bf388b3e87ba5e8a0011b6c89474fc46
ste <s.cadeck@gmail.com>
2021-05-01 10:19:40 +0200

    Update menu and carousel.js

--- js/carousel.js
+++ js/carousel.js
@@ -32,5 +32,30 @@
 // var ThumbailsWidth = ($('#image-slider').width() - 18.5) / 7;
 // $('#thumbnail li').find('img').css('width', ThumbailsWidth);
 
-
+$(document).ready(function() {
+    var $item = 0,
+      $itemNo = $(".hero figure").length;
+    function transitionSlide() {
+      $item++;
+      if ($item > $itemNo - 1) {
+        $item = 0;
+      }
+      $(".hero figure").removeClass("on");
+      $(".hero figure")
+        .eq($item)
+        .addClass("on");
+    }
+    var $autoTransition = setInterval(transitionSlide, 2500);
+  
+    $(".hero figure").click(function() {
+      clearInterval($autoTransition);
+      $item = $(this).index();
+      $(".hero figure").removeClass("on");
+      $(".hero figure")
+        .eq($item)
+        .addClass("on");
+      $autoTransition = setInterval(transitionSlide, 2500);
+    });
+  });
+  
 
