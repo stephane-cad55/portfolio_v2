89decbca4461a2dada6cee9610f891f37482cac8
ste <s.cadeck@gmail.com>
2021-05-02 11:46:15 +0200

    Update menu and scrollToTop.js, carousel.js

--- js/scrollToTop.js
+++ js/scrollToTop.js
@@ -1,13 +1,13 @@
 $('<div></div>')
     .attr('id', 'scrolltotop')
     .hide()
-    .css({ 'z-index': '1000', 'position': 'fixed', 'bottom': '15px', 'right': '20px', 'cursor': 'pointer', 'width': '50px', 'height': '50px', 'background': 'white' })
+    .css({ 'z-index': '1000', 'position': 'fixed', 'bottom': '20px', 'right': '20px', 'cursor': 'pointer', 'width': '50px', 'height': '50px', 'background': 'white', 'border-radius': '50px', })
     .appendTo('body')
     .click(function () {
         $('html,body').animate({ scrollTop: 0 }, 'slow');
     });
 $('<div></div>')
-    .css({ 'width': '10px', 'height': '10px', 'transform': 'rotate(-135deg)', 'border': 'solid black', 'border-width': '0 5px 5px 0', 'padding': '3px', 'margin-top': '20px', 'margin-left': '20px' })
+    .css({ 'width': '10px', 'height': '10px', 'transform': 'rotate(-135deg)', 'border': 'solid #15117c', 'border-width': '0 5px 5px 0', 'padding': '3px', 'margin-top': '20px', 'margin-left': '20px' })
     .appendTo('#scrolltotop');
 $(window).scroll(function () {
     if ($(window).scrollTop() < 10) {
